30e3282f69827f38a52f22aaf27fca98df2b3cfd
Kushagra <kushagraporwal2001@gmail.com>
2022-06-28 23:37:55 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
         <p>
          Hello defi
         </p>
