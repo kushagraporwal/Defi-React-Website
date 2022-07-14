b5e86d6f05ee0319c96838c81b2db7b1e6c10336
Kushagra <kushagraporwal2001@gmail.com>
2022-07-14 23:22:31 +0530

    another commit

--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Subscribe.css'
 const Subscribe = () => {
   return (
-    <div className='sucscribe'>
+    <div className='subscribe'>
         <div className='content'>
             <h2>Join our defi community</h2>
-            <form action="">
+            <form action="" method="POST">
                 <div className='form-container display-col'>
                     <input type="email" name="email" placeholder="Enter your email" />
                     <button className='btn'>Sign Up</button>
